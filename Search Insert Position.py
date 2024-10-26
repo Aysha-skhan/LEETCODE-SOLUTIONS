08ec70bbcbdda8017f483c6b96bbacba821bfc74
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2024-10-26 11:29:27 -0700

    Solution beats 100% Time complexity

---
+++ Search Insert Position.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for k in range(len(nums)):
+            if nums[k]==target:
+                return k
+            elif nums[k]< target and k+1<len(nums):
+                if nums[k+1]== target:
+                    return k+1
+                elif nums[k+1]> target:
+                    return k+1
+            elif nums[k]< target and k+1==len(nums):
+                return k+1
+        return 0
