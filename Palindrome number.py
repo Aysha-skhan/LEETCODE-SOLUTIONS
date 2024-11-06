d088b3bd13c1b45b00a00554f40a4e2bc5cc7898
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2024-11-06 23:58:24 +0500

    Palindrome number added!

---
+++ Palindrome number.py
@@ -0,0 +1,10 @@
+def isPalindrome(self, x: int) -> bool:
+        s=str(x)
+        stk=[]
+        for z in range(len(s)):
+            stk.append(s[z])
+        for m in range(len(s)):
+            v=stk.pop()
+            if s[m]!=v:
+                return False
+        return True
