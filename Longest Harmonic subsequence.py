db46d65155f0071e3507bda67979fc1f7ddfe867
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2024-12-03 23:17:50 +0500

    Solution uploaded!

---
+++ Longest Harmonic subsequence.py
@@ -0,0 +1,30 @@
+def findLHS(nums):
+    counts={}
+    for a in range(len(nums)):
+        if nums[a] not in counts:
+            counts[nums[a]]=1
+        else:
+            counts[nums[a]] += 1
+    # print(counts)
+    maxx=0
+    for key,val in counts.items():
+        if key<0:
+            check=-(abs(key)+1)
+            # print(check)
+            if check in counts:
+                # print('yeah')
+                if val+counts[check]>maxx:
+                    maxx=val+counts[check]
+            if key==-1 and (0 in counts):
+                if val+counts[0]>maxx:
+                    maxx=val+counts[0]
+        else:
+            check=key+1
+            # print(check)
+            if check in counts:
+                # print('yeah')
+                if val+counts[check]>maxx:
+                    maxx=val+counts[check]
+    return maxx
+
+print(findLHS([-1,-1,-1,0,0,1,-2,-2]))
