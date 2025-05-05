cedfab917b3278681e490412d62069a696df806f
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-05 21:40:23 +0500

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,13 +1,12 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        result=[]
-        def check(comb,left,right):
-            if left==n and right==n:
-                result.append(comb)
-                return
-            if left < n:
-                check(comb+'(',left+1,right)
-            if right < left:
-                check(comb+')',left,right+1)
-        check('',0,0)
-        return result
+def generateParenthesis(self, n: int) -> List[str]:
+    result=[]
+    def check(comb,left,right):
+        if left==n and right==n:
+            result.append(comb)
+            return
+        if left < n:
+            check(comb+'(',left+1,right)
+        if right < left:
+            check(comb+')',left,right+1)
+    check('',0,0)
+    return result
