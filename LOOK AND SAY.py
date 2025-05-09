227503bebeb7e260744788abebf992af119a8abe
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-09 22:59:01 +0500

    Add files via upload

---
+++ LOOK AND SAY.py
@@ -0,0 +1,9 @@
+def lookAndSequence(n,s='11'):
+    if n==0:
+        return s
+    else:
+        s2=''
+        s2+= str(s.count(s[0]))
+        return lookAndSequence(n-1,s)
+print(lookAndSequence(4))
+
