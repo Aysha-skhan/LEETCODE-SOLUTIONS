96c03a5c10559779081a2e5086cdad8307477a24
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-13 23:22:23 +0500

    Create RansomNote.py

---
+++ RansomNote.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d={}
+        for k in range(len(magazine)):
+            if magazine[k] not in d:
+                d[magazine[k]]=1
+            else:
+                d[magazine[k]]+=1
+        for y in range(len(ransomNote)):
+            if (ransomNote[y] not in d) or (ransomNote[y] == 0):
+                return False
+            else:
+                ransomNote[y]-=1
+        return True
