03435eaa7b388686dd8dcadc6092bc6e1a1c68cb
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-14 21:37:03 +0500

    Create CoinChange.py

---
+++ CoinChange.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_coins_needed=[amount+1]*(amount+1)
+        min_coins_needed[0]=0
+
+        for curr_amount in range(1,amount+1):
+            for curr_coin in coins:
+                if curr_amount - curr_coin >=0:
+                    min_coins_needed[curr_amount]=min(min_coins_needed[curr_amount],1+(min_coins_needed[curr_amount-curr_coin]))
+        if  min_coins_needed[-1]==(amount+1):
+            return -1
+        else:
+             return min_coins_needed[-1]
+        
