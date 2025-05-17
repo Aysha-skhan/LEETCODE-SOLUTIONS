1c87073eb3432ed3fd9119ed3a6dcaded4ce3c01
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-17 21:48:13 +0500

    Update SquareRoot(x).py

--- SquareRoot(x).py
+++ SquareRoot(x).py
@@ -1,16 +1,15 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        chk=x//2
-        if x==1:
-            return 1
-        if x==0:
-            return 0
-        while chk>0:
-            if chk*chk==x:
-                return chk
-            elif chk*chk>x:
-                chk=chk//2
-            elif chk*chk<x and ((chk+1)*(chk+1))<=x:
-                chk+=1
-            elif chk*chk<x and ((chk+1)*(chk+1))>x:
-                return chk
+def mySqrt(self, x: int) -> int:
+    chk=x//2
+    if x==1:
+        return 1
+    if x==0:
+        return 0
+    while chk>0:
+        if chk*chk==x:
+            return chk
+        elif chk*chk>x:
+            chk=chk//2
+        elif chk*chk<x and ((chk+1)*(chk+1))<=x:
+            chk+=1
+        elif chk*chk<x and ((chk+1)*(chk+1))>x:
+            return chk
