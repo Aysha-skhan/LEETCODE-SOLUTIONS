1ec0541e40a71959b2e05710e84b9c2a64c33e97
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-18 23:14:59 +0500

    Beats 100%

--- Pascal.py
+++ Pascal.py
@@ -1,15 +1,14 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        t=[[]]
-        for n in range(numRows):
-            if n==0:
-                t[0]+=[1]
-            elif n==1:
-                t.append([1,1])
-            else:
-                tmp=[1]
-                for y in range((len(t[-1]))-1):
-                    tmp+=[t[-1][y]+t[-1][y+1]]
-                tmp+=[1]
-                t+=[tmp]
-        return t
+def generate(self, numRows: int) -> List[List[int]]:
+    t=[[]]
+    for n in range(numRows):
+        if n==0:
+            t[0]+=[1]
+        elif n==1:
+            t.append([1,1])
+        else:
+            tmp=[1]
+            for y in range((len(t[-1]))-1):
+                tmp+=[t[-1][y]+t[-1][y+1]]
+            tmp+=[1]
+            t+=[tmp]
+    return t
