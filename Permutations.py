c4838d3633c70dbf3b1f68029147af6a2ad669d2
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-05-26 23:18:46 +0500

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,14 +1,13 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(start):
-            if start == len(nums):
-                result.append(nums[:])
-                return
-            for i in range(start, len(nums)):
-                nums[start], nums[i] = nums[i], nums[start]  # Swap
-                backtrack(start + 1)
-                nums[start], nums[i] = nums[i], nums[start]  # Backtrack
+def permute(self, nums: List[int]) -> List[List[int]]:
+    def backtrack(start):
+        if start == len(nums):
+            result.append(nums[:])
+            return
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]  # Swap
+            backtrack(start + 1)
+            nums[start], nums[i] = nums[i], nums[start]  # Backtrack
 
-        result = []
-        backtrack(0)
-        return result
+    result = []
+    backtrack(0)
+    return result
