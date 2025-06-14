87092029026c54c6de3b828122f8dd02b11d4b2a
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-06-14 11:51:20 +0500

    Update ShortestCompletingWord.py

--- ShortestCompletingWord.py
+++ ShortestCompletingWord.py
@@ -1,31 +1,30 @@
-
-    def shortestCompletingWord(self, licensePlate: str, words: List[str]):
-        d = {}
-        result = []
-        for y in range(len(licensePlate)):
-            if ord(licensePlate[y]) >= 65:
-                c = licensePlate[y].lower()
-                if c in d:
-                    d[c] += 1
-                else:
-                    d[c] = 1
-        # print(d)
-        for m in range(len(words)):
-            if len(words[m]) >= len(d):
-                # print('check-->',words[m])
-                chk = 0
-                for key, value in d.items():
-                    if words[m].count(key) < value:
-                        # print(words[m],key)
-                        chk = -1
-                        break
-                if chk ==0 :
-                    # print('yes')
-                    result.append(words[m])
-        minn = len(result[0])
-        ans = result[0]
-        for n in range(1, len(result)):
-            if len(result[n]) < minn:
-                minn = len(result[n])
-                ans = result[n]
-        return ans
+def shortestCompletingWord(self, licensePlate: str, words: List[str]):
+    d = {}
+    result = []
+    for y in range(len(licensePlate)):
+        if ord(licensePlate[y]) >= 65:
+            c = licensePlate[y].lower()
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+    # print(d)
+    for m in range(len(words)):
+        if len(words[m]) >= len(d):
+            # print('check-->',words[m])
+            chk = 0
+            for key, value in d.items():
+                if words[m].count(key) < value:
+                    # print(words[m],key)
+                    chk = -1
+                    break
+            if chk ==0 :
+                # print('yes')
+                result.append(words[m])
+    minn = len(result[0])
+    ans = result[0]
+    for n in range(1, len(result)):
+        if len(result[n]) < minn:
+            minn = len(result[n])
+            ans = result[n]
+    return ans
