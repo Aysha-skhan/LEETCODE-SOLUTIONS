e6dcd6baee064b896547cbf62c8e7f6fdf27af15
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-06-23 23:02:21 +0500

    Update Sort Colors.py

--- Sort Colors.py
+++ Sort Colors.py
@@ -1,7 +1,7 @@
-def sortColors(self, nums: List[int]) -> None:
+def sortColors(self, nums: List[int]):
     for k in range(len(nums)):
         for i in range(k+1,len(nums)):
             if nums[k]>nums[i]:
                 nums[k],nums[i]=nums[i],nums[k]
         
-        
+#in-place sorting was required
