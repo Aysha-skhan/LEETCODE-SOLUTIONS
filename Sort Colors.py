7ce1fa54ea15f93c4fe6e52e65fd8515fee9be26
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-06-23 23:01:59 +0500

    Update Sort Colors.py

--- Sort Colors.py
+++ Sort Colors.py
@@ -1,8 +1,7 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        for k in range(len(nums)):
-            for i in range(k+1,len(nums)):
-                if nums[k]>nums[i]:
-                    nums[k],nums[i]=nums[i],nums[k]
+def sortColors(self, nums: List[int]) -> None:
+    for k in range(len(nums)):
+        for i in range(k+1,len(nums)):
+            if nums[k]>nums[i]:
+                nums[k],nums[i]=nums[i],nums[k]
         
         
