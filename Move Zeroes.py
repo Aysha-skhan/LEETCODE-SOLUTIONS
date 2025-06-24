9dc3c6960d5ff3639ea38780d08af2ea68a1dff0
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2025-06-24 22:39:10 +0500

    Update Move Zeroes.py

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -1,10 +1,9 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        for y in range(len(nums)):
-            if nums[y]==0:
-                chk=y+1
-                while chk<len(nums):
-                    if nums[chk]!=0:
-                        nums[chk],nums[y]=nums[y],nums[chk]
-                        break
-                    chk+=1
+def moveZeroes(self, nums: List[int]) -> None:
+    for y in range(len(nums)):
+        if nums[y]==0:
+            chk=y+1
+            while chk<len(nums):
+                if nums[chk]!=0:
+                    nums[chk],nums[y]=nums[y],nums[chk]
+                    break
+                chk+=1
