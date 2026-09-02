a6678b5cbcefedd24a802fe8b08a43c9d2eaa177
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2026-09-02 23:39:23 +0500

    Fix in zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -2,19 +2,19 @@ def convert(self, s: str, numRows: int) -> str:
         if numRows==1 or numRows>=len(s):
             return s
         ans=['']*numRows
-        ind = 0
+        indx = 0
         for k in range(len(s)):
                 
-            if ind==0:
+            if indx==0:
                 flag=True
                 # print(ind)
-            elif ind==numRows-1:
+            elif indx==numRows-1:
                 flag=False
             if flag:
-                ans[ind]+=s[k]
+                ans[indx]+=s[k]
                 ind+=1
             else:
-                ans[ind] += s[k]
+                ans[indx] += s[k]
                 ind-=1
         answer=''
         for u in range(len(ans)):
