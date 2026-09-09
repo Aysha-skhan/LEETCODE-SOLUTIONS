7593f724f7e68233071070cae6e8615d79181975
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2026-09-09 23:50:18 +0500

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        tmp=str(n)
+        length=len(tmp)
+        if length<=3:
+            return 0
+        else:
+            commas=length//3
+            if commas>1 and length%3==0:
+                commas-=1
+            print(commas, "*", n-999)
+            return (commas)*(n-999)
+
+        
